83a16c92e1bea2d0487754057e3f119769b72ec6
satyam singhal <satyamsinghal248@gmail.com>
2025-02-01 11:24:14 +0530

    Solved 💡

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+       <Navbar/>
+      </div>
+    )
+  }
+}
+
+
